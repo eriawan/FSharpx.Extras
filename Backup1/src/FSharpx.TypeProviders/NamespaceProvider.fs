fe74864e4adcdb737da639ed9a3c8a33a9d0a7bf
eriawan <eriawan@live.com>
2016-10-16 15:23:10 +0700

    Convert to VIsual Studio 2015 Update 3 F# project format.

---
+++ Backup1/src/FSharpx.TypeProviders/NamespaceProvider.fs
@@ -0,0 +1,26 @@
+﻿module FSharpx.TypeProviders.NamespaceProvider
+
+open System.Reflection
+open Microsoft.FSharp.Core.CompilerServices
+open Samples.FSharp.ProvidedTypes
+open System.Text.RegularExpressions
+
+[<TypeProvider>]
+type public FSharpxProvider(cfg:TypeProviderConfig) as this =
+    inherit TypeProviderForNamespaces()
+
+    do this.AddNamespace(
+        Settings.rootNamespace, 
+        [RegexTypeProvider.regexTy
+         MiniCsvProvider.csvType cfg
+         FilesTypeProvider.typedFileSystem
+         XmlTypeProvider.xmlType this cfg
+         JsonTypeProvider.jsonType this cfg
+         RegistryProvider.typedRegistry
+         XamlProvider.xamlType this cfg
+         AppSettingsTypeProvider.typedAppSettings cfg
+         ExcelProvider.typExcel cfg
+         VectorTypeProvider.vectorTypeProvider])
+
+[<TypeProviderAssembly>]
+do ()
